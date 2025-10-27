a81ded182a23c1f622cbe84e244584a85c87afc5
mmorejon28279-cmyk <mmorejon28279@columbushs.com>
2025-10-27 12:51:18 -0400

    Finished unit 1.1

--- Units/Unit 1 - JavaScript Basics/1.1 - JavaScript Intro/Student/src/Lab1_1_JSIntro.js
+++ Units/Unit 1 - JavaScript Basics/1.1 - JavaScript Intro/Student/src/Lab1_1_JSIntro.js
@@ -26,8 +26,14 @@ const schoolName = "Columbus";
 console.log (schoolName)
 
 // TODO 5: Create three valid camelCase variables and print them
+let thisWayIsCalledCamelCase = "Day Of Week";
+console.log("value: ",thisWayIsCalledCamelCase, "Type: ", typeof thisWayIsCalledCamelCase)
 
+thisWayIsCalledCamelCase = "Day Of month";
+console.log("value: ",thisWayIsCalledCamelCase, "Type: ", typeof thisWayIsCalledCamelCase)
 
+thisWayIsCalledCamelCase = "Day Of Year";
+console.log("value: ",thisWayIsCalledCamelCase, "Type: ", typeof thisWayIsCalledCamelCase)
 // 🚀 Stretch Goals (optional)
 // - Create a variable age and print "I am X years old" using concatenation
 // - Repeat using template literals
@@ -35,7 +41,11 @@ console.log (schoolName)
 /* ===========================================================
    🎟 EXIT TICKET
    1. What new concept or skill did you learn in this lab?
+   I learned how to use console.log and create variables
    2. How did you apply logic or conditionals in your code?
+   I applied Logic by using common sense to understand that const stood for constant and it cant change.
    3. What challenges did you encounter, and how did you solve them?
+   One challenge i encountered was not understanding some terms like camelCase and i solved it by rereading the PDF.
    4. Where else could this programming concept be useful?
+   This could be useful when designing a professional website.
    =========================================================== */
