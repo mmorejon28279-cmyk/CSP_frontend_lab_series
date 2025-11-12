6b33d8a56272d96a138c2f6584b8615b291802c7
mmorejon28279-cmyk <mmorejon28279@columbushs.com>
2025-11-12 10:11:35 -0500

    Finished 2 tasks in tier 1 and 3 tasks in tier 2

--- Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.2/Practice_Activities/Tier01_BasicConditionals/Tier01_BasicConditionals.js
+++ Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.2/Practice_Activities/Tier01_BasicConditionals/Tier01_BasicConditionals.js
@@ -1,2 +1,27 @@
 // Your code goes here. Follow the steps in README.md and run from index.html.
 console.log("TEST - TIER 1")
+
+// grade evaluator
+// let grade = prompt("Enter your Grade")
+
+// if (grade >= 90) {
+//     console.log("A")
+// } else if (grade >= 80) {
+//     console.log("B")
+// } else if (grade >= 70) {
+//     console.log("C")
+// } else if (grade >= 60) {
+//     console.log("D")
+// } else {
+//     console.log("F")
+// } 
+
+// Voting eligibility
+// let age = prompt("How old are you")
+
+// if (age >= 18) {
+//     console.log("You can vote")
+// }
+// else {
+//     console.log("You can not vote")
+// }
--- Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.2/Practice_Activities/Tier02_Loops/Tier02_Loops.js
+++ Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.2/Practice_Activities/Tier02_Loops/Tier02_Loops.js
@@ -1,2 +1,30 @@
 // Your code goes here. Follow the steps in README.md and run from index.html.
 console.log("TEST - TIER 2")
+
+// Count to 10
+// for (let Number = 0; Number <= 10; Number++) { 
+//   console.log(Number); 
+// }
+
+// Countdown Timer
+// let T = 10;
+
+// while (T >= 0) { 
+//   if (T == 0) {
+//       console.log("Liftoff")
+//       T--;
+//     } else {
+//         console.log(T)
+//         T--;
+//     }
+// }
+
+//Password
+// let secret = "Explorers";
+// let pw
+// do {
+//     pw = prompt("What is the password?")
+// } while (pw != secret) ;
+
+
+// console.log("You are logged in.")
