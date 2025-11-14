5cdf979458d065edef86a403aee41b8062e1fa39
mmorejon28279-cmyk <mmorejon28279@columbushs.com>
2025-11-14 14:09:50 -0500

    finished 3 & 4

--- Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.2/Practice_Activities/Tier03_Combining/Tier03_Combining.js
+++ Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.2/Practice_Activities/Tier03_Combining/Tier03_Combining.js
@@ -1,2 +1,35 @@
 // Your code goes here. Follow the steps in README.md and run from index.html.
 console.log("TEST - TIER 3")
+
+//FizzBuzz
+// for (let i = 1; i <= 50; i++) {
+//     if (i % 15 === 0) {
+//         console.log("FizzBuzz");
+//     }
+//     else if (i % 3 === 0) {
+//         console.log("Fizz")
+//     }
+//     else if (i % 5 === 0) {
+//         console.log("Buzz");
+//     }
+//     else {
+//         console.log(i);
+//     }
+// }
+
+
+//Guess the number
+
+// let secret = Math.floor(Math.random() * 10) + 1;
+// let guess;
+// while (guess != secret) {
+//     guess = Number(prompt("Guess the Number 1-10"))
+//     if (guess < secret) {
+//        alert("Too low")
+//     }
+//     else if (guess > secret) {
+//         alert("Too High")
+//     } else {
+//         alert("Congrats!")
+//     }
+// }
--- Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.2/Practice_Activities/Tier04_Applications/Tier04_Applications.js
+++ Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.2/Practice_Activities/Tier04_Applications/Tier04_Applications.js
@@ -1,2 +1,21 @@
 // Your code goes here. Follow the steps in README.md and run from index.html.
 console.log("TEST - TIER 4")
+
+//login system 
+
+let u = "admin";
+let p = "1234";
+let username;
+let password;
+
+while (u != username && password != p) {
+  username = prompt("Enter username:");
+  password = prompt("Enter password:");
+
+  if (username === u && password === p) {
+    alert("Welcome!");
+    break;
+  } else {
+    alert("Invalid credentials. Try again.");
+  }
+}
