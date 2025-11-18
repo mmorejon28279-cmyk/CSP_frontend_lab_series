7b8d11aeeda12afe9d6efa111a27f8cdbb0028ad
mmorejon28279-cmyk <mmorejon28279@columbushs.com>
2025-11-18 14:05:21 -0500

    Update Tier04_Applications.js
    finished tier 4

--- Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.2/Practice_Activities/Tier04_Applications/Tier04_Applications.js
+++ Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.2/Practice_Activities/Tier04_Applications/Tier04_Applications.js
@@ -3,19 +3,34 @@ console.log("TEST - TIER 4")
 
 //login system 
 
-let u = "admin";
-let p = "1234";
-let username;
-let password;
+// let u = "admin";
+// let p = "1234";
+// let username;
+// let password;
 
-while (u != username && password != p) {
-  username = prompt("Enter username:");
-  password = prompt("Enter password:");
+// while (u != username && password != p) {
+//   username = prompt("Enter username:");
+//   password = prompt("Enter password:");
 
-  if (username === u && password === p) {
-    alert("Welcome!");
-    break;
-  } else {
-    alert("Invalid credentials. Try again.");
-  }
-}
+//   if (username === u && password === p) {
+//     alert("Welcome!");
+//     break;
+//   } else {
+//     alert("Invalid credentials. Try again.");
+//   }
+// }
+
+// weather bot
+// let weather = prompt("Whats the Weather?")
+
+// let weatherLowerCase = weather.toLowerCase();
+
+// if (weatherLowerCase == "sunny") {
+//   alert("T-shirt and Shorts");
+// } else if (weatherLowerCase == "rainy") {
+//   alert("raincoat, boots, and pants.");
+// } else if (weatherLowerCase == "cold") {
+//   alert("hoodie, pants, and sneakers.");
+// } else {
+//   alert("not a valid response");
+// }
